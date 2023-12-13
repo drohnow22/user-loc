ea390b72ba80e1efa4f34f9bf45240596bb13bd6
David Rohnow <davidrohnow@gmail.com>
2023-12-13 10:17:09 -0800

    P

---
+++ public/read.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Function to query information based on 
+ * a parameter: in this case, location.
+ *
+ */
+
+require "../config.php";
+require "../common.php";
+
+if (isset($_POST['submit'])) {
+  if (!hash_equals($_SESSION['csrf'], $_POST['csrf'])) die();
+
+  try  {
+    $connection = new PDO($dsn, $username, $password, $options);
+
+    $sql = "SELECT * 
+            FROM users
+            WHERE firstname = :firstname";
+
+    $firstname = $_POST['firstname'];
+    $statement = $connection->prepare($sql);
+    $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+    $statement->execute();
+
+    $result = $statement->fetchAll();
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+}
+?>
+<?php require "templates/header.php"; ?>
+        
+<?php  
+if (isset($_POST['submit'])) {
+  if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Results</h2>
+
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email Address</th>
+          <th>Age</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php foreach ($result as $row) : ?>
+        <tr>
+          <td><?php echo escape($row["id"]); ?></td>
+          <td><?php echo escape($row["firstname"]); ?></td>
+          <td><?php echo escape($row["lastname"]); ?></td>
+          <td><?php echo escape($row["email"]); ?></td>
+          <td><?php echo escape($row["age"]); ?></td>
+          <td><?php echo escape($row["location"]); ?></td>
+        </tr>
+      <?php endforeach; ?>
+      </tbody>
+    </table>
+    <?php } else { ?>
+      <blockquote>No results found for <?php echo escape($_POST['firstname']); ?>.</blockquote>
+    <?php } 
+} ?> 
+
+<h2>Find user</h2>
+
+<form method="post">
+  <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
+  <label for="firstname">name</label>
+  <input type="text" id="firstname" name="firstname">
+  <input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
